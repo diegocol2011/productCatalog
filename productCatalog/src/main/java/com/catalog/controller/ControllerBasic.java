a2661257e5604361ae0270d635c514285f45e216
diegocol2011 <diegocol2011@gmail.com>
2019-12-05 10:58:57 -0500

    Add library
    add library into folder "static"

--- productCatalog/src/main/java/com/catalog/controller/ControllerBasic.java
+++ productCatalog/src/main/java/com/catalog/controller/ControllerBasic.java
@@ -1,22 +1,13 @@
 package com.catalog.controller;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.constraints.Positive;
-
-import org.hibernate.validator.internal.util.privilegedactions.NewInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework. stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 
